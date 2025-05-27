1e0fc2562cf3a01936a632e4ff7a1cd909d91cae
María Macías <mariamdel06@gmail.com>
2025-05-27 12:41:13 +0200

    Add resta

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import utilitiesA as UtilA
+def resta(a,b):
+    d = a - b
+    return d
+
 def main():
     a = 7 
     b = 3
     c = UtilA.sumaA(a, b)
+    d = resta(a,b)
     print(f"La suma de {a} y {b} es :{c}")
+    print(f"Resultado de resta: {d}")
 
+
+    
 if __name__ == "__main__":
     main()
+
+
