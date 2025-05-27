0d898f124721a25e43d428b957f91e9da0265553
lourdescabezaolias@gmail.com <lourdes@MacBook-Pro-de-Lourdes.local>
2025-05-27 12:54:08 +0200

    Multiplicacion añadida

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import utilitiesA as UtilA
+def multiplicar(a,b):
+    c= a*b
+    return c
+
 def main():
     a = 7 
     b = 3
     c = UtilA.sumaA(a, b)
+    d= multiplicar(a,b)
     print(f"La suma de {a} y {b} es :{c}")
+    print(f"la multiplicación tiene como resultado {d}")
 
 if __name__ == "__main__":
     main()
+
